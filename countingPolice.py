7e454988ffe85bc6f43af6e3ad5a2c7a829d2a5b
pure-life-git <roscoegoodman02@gmail.com>
2020-12-09 20:41:03 -0800

    Update countingPolice.py

--- countingPolice.py
+++ countingPolice.py
@@ -16,11 +16,11 @@ count = []
 games = []
 
 def countEntry(num):
-    cur.execute("UPDATE countingtable SET count = ?;", (num))
+    cur.execute("UPDATE countingtable SET count = %s;", (num))
     conn.commit()
 
 def gameEntry(game):
-    cur.execute("INSERT INTO countingtable (games) VALUES (?);", (game))
+    cur.execute("INSERT INTO countingtable (games) VALUES (%s);", (game))
     conn.commit()
 
 @bot.event
