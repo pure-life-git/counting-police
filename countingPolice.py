86f3e6c28422e4cac30648acf31674e9bbc41649
pure-life-git <roscoegoodman02@gmail.com>
2020-12-12 16:53:13 -0800

    added .strikes in help

--- countingPolice.py
+++ countingPolice.py
@@ -46,6 +46,7 @@ async def on_ready():
 async def gamehelp(ctx):
     helpEmbed = discord.Embed(title='Help', description = 'Help with the bot')
     helpEmbed.add_field(name=".decide", value='Decide takes two arguments and generates a random number in the range of the two arguments', inline=False)
+    helpEmbed.add_field(name=".strikes", value='Lets you know how many strikes you have in the counting channel', inline=False)
     helpEmbed.add_field(name=".game", value='Game will add a game to the Game List. Simply type the command and then\nthe game you would like to add', inline=False)
     helpEmbed.add_field(name=".gameremove", value='Gameremove removes a game from the Game List. Simply type the command then\nthe game you would like to add', inline=False)
     helpEmbed.add_field(name=".choosegame", value='Choosegame will randomly choose a game from the Game List', inline=False)
