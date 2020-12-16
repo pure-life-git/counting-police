fdc29acb4bb03d7fe7dec62dfe5961d9af3b86e9
pure-life-git <roscoegoodman02@gmail.com>
2020-12-15 19:29:01 -0800

    Update countingPolice.py

--- countingPolice.py
+++ countingPolice.py
@@ -148,7 +148,9 @@ async def gamelist(ctx):
     games = []
     for i in rawList:
         games.append(i[0])
-    await ctx.send('\n'.join('**{}**: {}'.format(*k) for k in enumerate(games,1)))
+    gamelistEmbed = discord.Embed(title="Game List", description="List of games entered")
+    gamelistEmbed.add_field(name="Games",value='\n'.join('**{}**: {}'.format(*k) for k in enumerate(games,1)))
+    await ctx.send(embed = gamelistEmbed)
 
 @bot.command()
 async def gameremove(ctx,*,arg):
