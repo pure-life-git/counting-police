a1367ae8e3054b93f0706b37efc304ba6e8535cc
pure-life-git <roscoegoodman02@gmail.com>
2020-12-18 14:16:18 -0800

    Update countingPolice.py

--- countingPolice.py
+++ countingPolice.py
@@ -185,6 +185,7 @@ async def poll(ctx,*args):
         await m.add_reaction('✅')
         await m.add_reaction('❌')
         await asyncio.sleep(10)
+        reactions = m.reactions()
         yesResult = m.reactions.count('✅')
         print('yesresult='+str(yesResult))
         noResult = m.reactions.count('❌')
