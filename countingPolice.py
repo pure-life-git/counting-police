6b3cd16b6e791d62d0ee3333d93f034e5fd38920
pure-life-git <roscoegoodman02@gmail.com>
2020-12-21 01:02:39 -0800

    Update countingPolice.py

--- countingPolice.py
+++ countingPolice.py
@@ -161,7 +161,7 @@ async def play(ctx,url: str):
         if file.endswith(".mp3"):
             os.rename(file, "song.mp3")
     voice.play(discord.FFmpegPCMAudio("song.mp3"))
-    voice.volume = 100
+    voice.volume = 50
     voice.is_playing()
 
     
