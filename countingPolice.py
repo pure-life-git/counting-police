04dbb2249eed0692c77d253b4b90c3fdd095eefa
pure-life-git <roscoegoodman02@gmail.com>
2021-01-05 23:02:04 -0800

    Update countingPolice.py

--- countingPolice.py
+++ countingPolice.py
@@ -1687,14 +1687,14 @@ async def pointtable(ctx):
     for elt in cur.description:
         col_names.append(elt[0])
     
-    table = "```+------------+---------+------------------+-----------+--------+\n|pointnumber |name     |id                |bj wins    |totalpoints |\n+------------+---------+------------------+-----------+--------+\n"
+    table = "```+------------+---------+------------------+-----------+--------+\n|pointnumber |name     |id                |bj wins    |totalpoints |\n+------------+---------+------------------+-----------+------------+\n"
 
     for row in rows:
         entry = f"|{row[0]:<12}|{row[1]:<9}|{row[2]:<18}|{row[3]:<11}|{row[4]:<12}|\n"
         table = "".join((table,entry))
         #await ctx.send(f"`|{row[0]:<7}|{row[1]:<9}|{row[2]:<18}|{row[3]:<11}|{row[4]:<8}|`")
     
-    end = "+------------+---------+------------------+-----------+--------+```"
+    end = "+------------+---------+------------------+-----------+------------+```"
     table = "".join((table,end))
     print(len(table))
     await ctx.send(table)
