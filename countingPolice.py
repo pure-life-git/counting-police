dcfd55f61db16eb68996775e1be97f02fd4514ef
pure-life-git <roscoegoodman02@gmail.com>
2021-01-05 22:18:07 -0800

    Update countingPolice.py

--- countingPolice.py
+++ countingPolice.py
@@ -1680,17 +1680,17 @@ async def pointtable(ctx):
     if ctx.author.id not in modID:
         return
     col_names = []
-    SQL = "SELECT * FROM points;"
+    SQL = "SELECT pointnumber, name, id, bjwins,totalpoints FROM points;"
     cur.execute(SQL)
     rows = cur.fetchall()
 
     for elt in cur.description:
         col_names.append(elt[0])
     await ctx.send("`+----------------------"+len(col_names)*'+----------------------+'+'`')
-    await ctx.send("`|pointnumber           |name                  |id                    |claimtime             |blackjack wins        |total points          |`")
+    await ctx.send("`|pointnumber           |name                  |id                    |blackjack wins        |total points          |`")
     await ctx.send("`+----------------------"+len(col_names)*'+----------------------+`')
     for row in rows:
-        await ctx.send(f"`|{row[0]:<22}|{row[1]:<22}|{row[2]:<22}|{row[3]:<22}|{row[4]:<22}|{row[5]:<22}|`")
+        await ctx.send(f"`|{row[0]:<22}|{row[1]:<22}|{row[2]:<22}|{row[3]:<22}|{row[4]:<22}|`")
     await ctx.send("`+----------------------"+len(col_names)*'+----------------------+'+'`')
 
 
