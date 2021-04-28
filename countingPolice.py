a8b0cf0040d2d07b1e7b86817bc0bbd13904dcfc
pure-life-git <roscoegoodman02@gmail.com>
2021-04-28 01:15:41 -0700

    Update countingPolice.py

--- countingPolice.py
+++ countingPolice.py
@@ -1212,10 +1212,11 @@ async def play_music(ctx,song):
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([song])
     
+    
+    # await ctx.send(f"**Now Playing:** {title} - {channel} | {runtime}")
+    voice.play(FFmpegPCMAudio(source="song.mp3"), after = lambda e: asyncio.run_coroutine_threadsafe(check_play_next(ctx)))
     np_embed = discord.Embed(title="Now Playing", description=f"`{title}` requested by {author.mention}", value=f"Duration: {runtime}", color=bot_color)
     await ctx.send(embed=np_embed)
-    # await ctx.send(f"**Now Playing:** {title} - {channel} | {runtime}")
-    voice.play(FFmpegPCMAudio(source="song.mp3"), after = lambda e: asyncio.run_coroutine_threadsafe(check_play_next(ctx), bot.loop))
 
 @bot.command(name="play", description="Plays a song in a voice channel", aliases=["p"])
 async def play(ctx, *args):
