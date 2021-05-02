5a4a8f2f69cd293bc3bd8bb51a3feb80e014898d
pure-life-git <roscoegoodman02@gmail.com>
2021-05-02 04:18:02 -0700

    Update countingPolice.py

--- countingPolice.py
+++ countingPolice.py
@@ -1302,8 +1302,7 @@ async def skip(ctx):
     if voice.is_connected():
         if voice.is_playing():
             if len(music_queue)>0:
-                voice.stop()
-                await play_music(ctx,music_queue.pop(0))
+               bot.loop.create_task(play_music(ctx,music_queue.pop(0)))
             else:
                 voice.stop()
         else:
