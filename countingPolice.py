fd3a5684d54011ffb7b0a2115ff542e483498551
pure-life-git <roscoegoodman02@gmail.com>
2021-05-02 03:28:11 -0700

    Update countingPolice.py

--- countingPolice.py
+++ countingPolice.py
@@ -1215,7 +1215,8 @@ async def play_music(ctx,song):
     
     
     # await ctx.send(f"**Now Playing:** {title} - {channel} | {runtime}")
-    voice.play(discord.FFmpegPCMAudio(source="song.mp3"),after=lambda e: asyncio.create_task(check_play_next(ctx), name="play song"))
+    loop = voice.loop
+    voice.play(discord.FFmpegPCMAudio(source="song.mp3"),after=lambda e: loop.create_task(check_play_next(ctx), name="play song"))
     print("played audio...")
     np_embed = discord.Embed(title="Now Playing", description=f"`{title}` requested by {author.mention}", value=f"Duration: {runtime}", color=bot_color)
     await ctx.send(embed=np_embed)
