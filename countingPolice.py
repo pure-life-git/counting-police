74c39bff818c2519834e86b8cd8b8fc0f5ffadf8
pure-life-git <roscoegoodman02@gmail.com>
2021-05-02 02:52:57 -0700

    Update countingPolice.py

--- countingPolice.py
+++ countingPolice.py
@@ -1216,7 +1216,7 @@ async def play_music(ctx,song):
     
     # await ctx.send(f"**Now Playing:** {title} - {channel} | {runtime}")
     try:
-        task = voice.play(discord.FFmpegPCMAudio(source="song.mp3"),after=lambda e: asyncio.ensure_future(check_play_next(ctx)))
+        task = voice.play(discord.FFmpegPCMAudio(source="song.mp3"),after=lambda e: asyncio.ensure_future(check_play_next(ctx), bot.loop))
     except RuntimeError:
         print("runtime error")
     print("played audio...")
